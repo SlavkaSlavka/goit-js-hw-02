c5e1589ea479c441b8775da0fdffa2a24509cdca
SlavkaSlavka <56847146+SlavkaSlavka@users.noreply.github.com>
2021-02-08 17:59:59 +0200

    таск 1-6

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,27 +1,32 @@
-const calculateEngravingPrice = function (message, pricePerWord) {
-  return message.split(' ').length * pricePerWord;
+const calculateEngravingPrice = function(message, pricePerWord) {
+ const messageLength = message.split(' ').length; 
+ // можно было не делать переменную, а записать const costEngraving = pricePerWord * message.split(' ').length;
+ // но я подумал, так понятней :)
+ const costEngraving = pricePerWord * messageLength;
+ return costEngraving;
 };
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
 console.log(
-  'Full cost: ',
   calculateEngravingPrice(
     'Proin sociis natoque et magnis parturient montes mus',
     10,
   ),
-);
+); // 80
+
 console.log(
-  'Full cost: ',
   calculateEngravingPrice(
     'Proin sociis natoque et magnis parturient montes mus',
     20,
   ),
-);
+); // 160
 
 console.log(
-  'Full cost: ',
   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-);
+); // 240
 
 console.log(
-  'Full cost: ',
   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-);
+); // 120
--- JS/task-5.js
+++ JS/task-5.js
@@ -1,15 +1,17 @@
+
 //Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
-// Функция проверяет ее на содержание слов spam и sale. 
+//Функция проверяет ее на содержание слов spam и sale. 
 //Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. 
 //Слова в строке могут быть в произвольном регистре.
 let spam = false;
-
 const checkForSpam = function(message) {
-  let normalizeMesasge = message.toLowerCase();
-  if (normalizeMesasge.includes('spam') || normalizeMesasge.includes('sale')) {
-  spam = true;} 
+  const normalizedMessage = message.toLowerCase();
+  if (normalizedMessage.includes('spam') || normalizedMessage.includes('sale')) {
+    spam = true;
+  }
   return spam;
-};
+}
+ 
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
